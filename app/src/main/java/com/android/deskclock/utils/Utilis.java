f78c7faa367827ee4cba69df7113ed3cf7f4097c
zen kun <zenkun@zebstudios.com>
2014-12-04 19:31:54 -0600

    first approach

---
+++ app/src/main/java/com/android/deskclock/utils/Utilis.java
@@ -0,0 +1,72 @@
+package com.android.deskclock.utils;
+
+import android.os.Build;
+
+/**
+ * Created by zenkun on 12/4/14.
+ */
+public class Utilis {
+    public static boolean isExactlyICS() {
+        // Can use static final constants like ICS, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed
+        // behavior.
+        return Build.VERSION.SDK_INT == Build.VERSION_CODES.ICE_CREAM_SANDWICH
+                || Build.VERSION.SDK_INT == Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1;
+    }
+
+    public static boolean isICS() {
+        // Can use static final constants like ICS, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed
+        // behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+    }
+
+    public static boolean isICSMR1() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1;
+    }
+
+    public static boolean isJB() {
+        // Can use static final constants like ICS, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed
+        // behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+    }
+    public static boolean isJBMR1() {
+        // Can use static final constants like ICS, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed
+        // behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1;
+    }
+    public static boolean isJBMR2() {
+        // Can use static final constants like ICS, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed
+        // behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2;
+    }
+    public static boolean isKK() {
+        // Can use static final constants like ICS, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed
+        // behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    }
+    public static boolean isLP() {
+        // Can use static final constants like ICS, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed
+        // behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+
+    public static boolean isGB() {
+        // Can use static final constants like ICS, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed
+        // behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
+    }
+
+    public static boolean isHoneyComb() {
+        // Can use static final constants like ICS, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed
+        // behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+    }
+}
